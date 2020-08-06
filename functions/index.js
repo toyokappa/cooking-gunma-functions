b18e12b23d0dbcaa3fdc228dd0dc05ac56993099
Yuta Toyokawa <kppg42@gmail.com>
2020-08-06 19:19:53 +0900

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,35 @@
+const functions = require('firebase-functions');
+const mailgun = require('mailgun-js')
+const apiKey = functions.config().mail.api_key
+const domain = functions.config().mail.domain
+const mailTo = functions.config().mail.to
+const mailBcc = functions.config().mail.bcc
+
+const mg = mailgun({ apiKey, domain })
+
+const mailTemplate = (data) => {
+  return `以下の内容でホームページよりお問い合わせがありました。
+【お問い合わせ種別】 ${data.category}
+【お名前】 ${data.name}
+【連絡先】 ${data.email}
+【内容】
+ ${data.message} 
+  `
+}
+
+exports.sendMail = functions.https.onCall(async (data, _context) => {
+  const mail = {
+    from: 'no-reply@cooking-gunma.club',
+    to: mailTo,
+    bcc: mailBcc,
+    subject: '【群馬お料理クラブ】お問い合わせがありました',
+    text: mailTemplate(data)
+  }
+
+  try {
+    await mg.messages().send(mail)
+  } catch (e) {
+    console.error(`send faild: ${e}`)
+    throw new functions.https.HttpsError('internal', `send faild: ${e}`)
+  }
+})
